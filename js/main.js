df0396ce22e77b66ad5f7f7f7709e41d95953fc4
Danith Thai <braddong@hotmail.com>
2018-12-05 09:53:39 +1100

    resume button, projects bbuttons added.  still resizing issue

--- js/main.js
+++ js/main.js
@@ -1,33 +1,68 @@
 $(document).ready(function(){
 
   $('#contactDetails').hide();
-
   $('#contactButton').on('click', function(){
+    $('#aboutMeContent').hide(600);
+    $('#proj1').hide(600);
+    $('#proj2').hide(600);
+    $('#proj3').hide(600);
+    $('#proj4').hide(600);
     $('#contactDetails').toggle(1000);
   })
 
 
   $('#aboutMeContent').hide();
   $('#aboutMeButton').on('click', function(){
+    $('#contactDetails').hide(600);
+    $('#proj1').hide(600);
+    $('#proj2').hide(600);
+    $('#proj3').hide(600);
+    $('#proj4').hide(600);
     $('#aboutMeContent').toggle(1000);
   });
 
 
 
-  // $('#proj1').hide();
-  // $('#TTT').on('click', function(){
-  //   $('#proj1').toggle(1000);
-  // })
+  $('#proj1').hide();
+  $('#ttt').on('click', function(){
+    $('#aboutMeContent').hide(600);
+    $('#contactDetails').hide(600);
+    $('#proj2').hide(600);
+    $('#proj3').hide(600);
+    $('#proj4').hide(600);
+    $('#proj1').toggle(1000);
+  })
 
 
   $('#proj2').hide();
-
+  $('#conflicts').on('click', function(){
+    $('#aboutMeContent').hide(600);
+    $('#contactDetails').hide(600);
+    $('#proj1').hide(600);
+    $('#proj3').hide(600);
+    $('#proj4').hide(600);
+    $('#proj2').toggle(1000);
+  })
 
   $('#proj3').hide();
-
+  $('#trippo').on('click', function(){
+    $('#aboutMeContent').hide(600);
+    $('#contactDetails').hide(600);
+    $('#proj1').hide(600);
+    $('#proj2').hide(600);
+    $('#proj4').hide(600);
+    $('#proj3').toggle(1000);
+  })
 
   $('#proj4').hide();
-
+  $('#booker').on('click', function(){
+    $('#aboutMeContent').hide(600);
+    $('#contactDetails').hide(600);
+    $('#proj1').hide(600);
+    $('#proj2').hide(600);
+    $('#proj3').hide(600);
+    $('#proj4').toggle(1000);
+  })
 
 
 }); //end document ready
