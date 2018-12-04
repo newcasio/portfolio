8a0d2bfff7263ab0e517620ced7af3dd85d80bac
Danith Thai <braddong@hotmail.com>
2018-12-04 22:27:05 +1100

    projects slides working.  need to link to button in nav

--- js/main.js
+++ js/main.js
@@ -12,4 +12,22 @@ $(document).ready(function(){
     $('#aboutMeContent').toggle(1000);
   });
 
+
+
+  // $('#proj1').hide();
+  // $('#TTT').on('click', function(){
+  //   $('#proj1').toggle(1000);
+  // })
+
+
+  $('#proj2').hide();
+
+
+  $('#proj3').hide();
+
+
+  $('#proj4').hide();
+
+
+
 }); //end document ready
