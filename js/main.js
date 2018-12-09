acdc2123abe300f55c31dca079bde532def8c9ab
Brad Dong <braddong@hotmail.com>
2018-12-10 08:14:52 +1100

    background image fade on button press

--- js/main.js
+++ js/main.js
@@ -1,84 +1,93 @@
 $(document).ready(function(){
 
+  $('.bg-img').css('opacity', '1');
+
   $('#contactDetails').hide();
   $('#contactButton').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj1').hide(600);
-    $('#proj2').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').hide(600);
-    $('#contactDetails').toggle(1000);
+    $('#aboutMeContent').hide();
+    $('#skillsContent').hide();
+    $('#proj1').hide();
+    $('#proj2').hide();
+    $('#proj3').hide();
+    $('#proj4').hide();
+    $('#contactDetails').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   })
-
 
   $('#aboutMeContent').hide();
   $('#aboutMeButton').on('click', function(){
-    $('#contactDetails').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj1').hide(600);
-    $('#proj2').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').hide(600);
-    $('#aboutMeContent').toggle(1000);
+    $('#contactDetails').hide();
+    $('#skillsContent').hide();
+    $('#proj1').hide();
+    $('#proj2').hide();
+    $('#proj3').hide();
+    $('#proj4').hide();
+    $('#aboutMeContent').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
+
   });
 
   $('#skillsContent').hide();
   $('#skills').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#contactDetails').hide(600);
-    $('#proj1').hide(600);
-    $('#proj2').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').hide(600);
-    $('#skillsContent').toggle(1000);
+    $('#aboutMeContent').hide();
+    $('#contactDetails').hide();
+    $('#proj1').hide();
+    $('#proj2').hide();
+    $('#proj3').hide();
+    $('#proj4').hide();
+    $('#skillsContent').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   });
 
 
 
   $('#proj1').hide();
   $('#ttt').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#contactDetails').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj2').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').hide(600);
-    $('#proj1').toggle(1000);
+    $('#aboutMeContent').hide();
+    $('#contactDetails').hide();
+    $('#skillsContent').hide();
+    $('#proj2').hide();
+    $('#proj3').hide();
+    $('#proj4').hide();
+    $('#proj1').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   })
 
 
   $('#proj2').hide();
   $('#conflicts').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#contactDetails').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj1').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').hide(600);
-    $('#proj2').toggle(1000);
+    $('#aboutMeContent').hide();
+    $('#contactDetails').hide();
+    $('#skillsContent').hide();
+    $('#proj1').hide();
+    $('#proj3').hide();
+    $('#proj4').hide();
+    $('#proj2').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   })
 
   $('#proj3').hide();
   $('#trippo').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#contactDetails').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj1').hide(600);
-    $('#proj2').hide(600);
-    $('#proj4').hide(600);
-    $('#proj3').toggle(1000);
+    $('#aboutMeContent').faddeOut(200);
+    $('#contactDetails').faddeOut(200);
+    $('#skillsContent').faddeOut(200);
+    $('#proj1').faddeOut(200);
+    $('#proj2').faddeOut(200);
+    $('#proj4').faddeOut(200);
+    $('#proj3').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   })
 
   $('#proj4').hide();
   $('#booker').on('click', function(){
-    $('#aboutMeContent').hide(600);
-    $('#contactDetails').hide(600);
-    $('#skillsContent').hide(600);
-    $('#proj1').hide(600);
-    $('#proj2').hide(600);
-    $('#proj3').hide(600);
-    $('#proj4').toggle(1000);
+    $('#aboutMeContent').hide();
+    $('#contactDetails').hide();
+    $('#skillsContent').hide();
+    $('#proj1').hide();
+    $('#proj2').hide();
+    $('#proj3').hide();
+    $('#proj4').fadeToggle(1800);
+    $('.bg-img').fadeTo('slow', '0.3');
   })
 
 
